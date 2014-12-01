6c1bda99a1c187fb346b37a9cf4194fbbbbb983f
mikoto2000 <mikoto2000@gmail.com>
2014-12-02 00:27:13 +0900

    Added Add methods test.

--- _test/OzClassDiagram_test.go
+++ _test/OzClassDiagram_test.go
@@ -30,6 +30,20 @@ func TestFields(t *testing.T) {
 	}
 }
 
+func TestFieldsAdd(t *testing.T) {
+	defs := []string{"- field01 : string"}
+	def := "- field02 : string"
+	fields := cld.CreateFieldsFromStrings(defs)
+	fields.Add(cld.CreateFieldFromString(def))
+
+	actual := fields.ToDot()
+	expected := defs[0] + "\\l" + def + "\\l"
+
+	if expected != actual {
+		t.Errorf("got \"%s\" but want \"%s\"", actual, expected)
+	}
+}
+
 func getFieldsDefs() []string {
 	return []string{"- f1 : string", "- f2 : string", "- f3 : string"}
 }
@@ -57,6 +71,20 @@ func TestMethods(t *testing.T) {
 
 	actual := methods.ToDot()
 	expected := defs[0] + "\\l" + defs[1] + "\\l"
+
+	if expected != actual {
+		t.Errorf("got \"%s\" but want \"%s\"", actual, expected)
+	}
+}
+
+func TestMethodsAdd(t *testing.T) {
+	defs := []string{"- method01() : string"}
+	def := "- method02() : string"
+	methods := cld.CreateMethodsFromStrings(defs)
+	methods.Add(cld.CreateMethodFromString(def))
+
+	actual := methods.ToDot()
+	expected := defs[0] + "\\l" + def + "\\l"
 
 	if expected != actual {
 		t.Errorf("got \"%s\" but want \"%s\"", actual, expected)
@@ -103,6 +131,19 @@ func TestNamespace(t *testing.T) {
 	namespace := cld.CreateNamespace("TestNamespace", []*cld.Class{
 		cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()),
 		cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs())})
+
+	actual1 := namespace.ToDot()
+	expected1 := "subgraph cluster_TestNamespace {\nlabel = \"TestNamespace\";\nTestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nTestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
+
+	if expected1 != actual1 {
+		t.Errorf("got\n \"%s\"\nbut want\n \"%s\"", actual1, expected1)
+	}
+}
+
+func TestNamespaceAddClass(t *testing.T) {
+	namespace := cld.CreateNamespace("TestNamespace", nil)
+	namespace.AddClass(cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()))
+	namespace.AddClass(cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs()))
 
 	actual1 := namespace.ToDot()
 	expected1 := "subgraph cluster_TestNamespace {\nlabel = \"TestNamespace\";\nTestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nTestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
