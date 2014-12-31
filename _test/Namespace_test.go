40f225acc0993d2a38cf837dd9e2a91d6614f5c7
mikoto2000 <mikoto2000@gmail.com>
2015-01-01 01:34:18 +0900

    Namespace の入れ子に対応しきれていなかった部分を修正。
    ノードの識別子がグローバルで一意となる必要があった
     -> 親名前空間の識別子をプレフィックスとして追加する
       - Class, Namespace に parent フィールドを追加
       - Class, Namespace に GetIdent(), GetParentIdent() を追加
       - Create, Add メソッド内で、必要であれば親情報を更新する処理を追加

--- _test/Namespace_test.go
+++ _test/Namespace_test.go
@@ -6,15 +6,27 @@ import (
 )
 
 func TestNamespace(t *testing.T) {
-	namespace := cld.CreateNamespace("TestNamespace", []*cld.Class{
+	namespace1 := cld.CreateNamespace("TestNamespace", []*cld.Class{
 		cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()),
 		cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs())}, nil)
 
-	actual1 := namespace.ToDot()
-	expected1 := "subgraph cluster_TestNamespace {\nlabel = \"TestNamespace\";\nTestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nTestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
+	actual1 := namespace1.ToDot()
+	expected1 := "subgraph cluster_main_TestNamespace {\nlabel = \"TestNamespace\";\nmain_TestNamespace_TestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nmain_TestNamespace_TestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
 
 	if expected1 != actual1 {
 		t.Errorf("got\n \"%s\"\nbut want\n \"%s\"", actual1, expected1)
+	}
+
+	namespace2 := cld.CreateNamespace("TestNamespace", []*cld.Class{
+		cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()),
+		cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs())},
+		[]*cld.Namespace{getTestNamespace("TestNamespace_innerNamespace")})
+
+	actual2 := namespace2.ToDot()
+	expected2 := "subgraph cluster_main_TestNamespace {\nlabel = \"TestNamespace\";\nsubgraph cluster_main_TestNamespace_TestNamespace_innerNamespace {\nlabel = \"TestNamespace_innerNamespace\";\nmain_TestNamespace_TestNamespace_innerNamespace_TestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nmain_TestNamespace_TestNamespace_innerNamespace_TestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}\nmain_TestNamespace_TestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nmain_TestNamespace_TestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
+
+	if expected2 != actual2 {
+		t.Errorf("got\n \"%s\"\nbut want\n \"%s\"", actual2, expected2)
 	}
 }
 
@@ -24,9 +36,16 @@ func TestNamespaceAddClass(t *testing.T) {
 	namespace.AddClass(cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs()))
 
 	actual1 := namespace.ToDot()
-	expected1 := "subgraph cluster_TestNamespace {\nlabel = \"TestNamespace\";\nTestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nTestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
+	expected1 := "subgraph cluster_main_TestNamespace {\nlabel = \"TestNamespace\";\nmain_TestNamespace_TestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nmain_TestNamespace_TestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
 
 	if expected1 != actual1 {
 		t.Errorf("got\n \"%s\"\nbut want\n \"%s\"", actual1, expected1)
 	}
+}
+
+func getTestNamespace(name string) *cld.Namespace {
+	namespace := cld.CreateNamespace(name, []*cld.Class{
+		cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()),
+		cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs())}, nil)
+	return namespace
 }
--- _test/Relation_test.go
+++ _test/Relation_test.go
@@ -6,10 +6,12 @@ import (
 )
 
 func TestRelation(t *testing.T) {
-	relation := cld.CreateRelation("RelationName", cld.RELATION_INHERIT, "TestClass1", "TestClass2", "fromMultiplicity", "toMultiplicity")
+	c1 := cld.CreateClassFromDefs("", "TestClass1", nil, nil)
+	c2 := cld.CreateClassFromDefs("", "TestClass2", nil, nil)
+	relation := cld.CreateRelation("RelationName", cld.RELATION_INHERIT, c1, c2, "fromMultiplicity", "toMultiplicity")
 
 	actual1 := relation.ToDot()
-	expected1 := "edge [style = \"solid\", arrowhead = \"onormal\"];\nTestClass1 -> TestClass2[label = \"RelationName\",taillabel = \"fromMultiplicity\",headlabel = \"toMultiplicity\"];"
+	expected1 := "edge [style = \"solid\", arrowhead = \"onormal\"];\nmain_TestClass1 -> main_TestClass2[label = \"RelationName\",taillabel = \"fromMultiplicity\",headlabel = \"toMultiplicity\"];"
 
 	if expected1 != actual1 {
 		t.Errorf("got\n \"%s\"\nbut want\n \"%s\"", actual1, expected1)
