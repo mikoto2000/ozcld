be2438028a6b35bad03ce2736e9ea9c8c3e144d0
mikoto2000 <mikoto2000@gmail.com>
2014-12-31 22:58:49 +0900

    Fixed test break.

--- _test/OzClassDiagram_test.go
+++ _test/OzClassDiagram_test.go
@@ -130,7 +130,7 @@ func TestMain(t *testing.T) {
 func TestNamespace(t *testing.T) {
 	namespace := cld.CreateNamespace("TestNamespace", []*cld.Class{
 		cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()),
-		cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs())})
+		cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs())}, nil)
 
 	actual1 := namespace.ToDot()
 	expected1 := "subgraph cluster_TestNamespace {\nlabel = \"TestNamespace\";\nTestClass1 [label = \"{TestClass1|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\nTestClass2 [label = \"{TestClass2|- f1 : string\\l- f2 : string\\l- f3 : string\\l|- m1() : string\\l- m2() : string\\l- m3() : string\\l}\"];\n}"
@@ -141,7 +141,7 @@ func TestNamespace(t *testing.T) {
 }
 
 func TestNamespaceAddClass(t *testing.T) {
-	namespace := cld.CreateNamespace("TestNamespace", nil)
+	namespace := cld.CreateNamespace("TestNamespace", nil, nil)
 	namespace.AddClass(cld.CreateClassFromDefs("", "TestClass1", getFieldsDefs(), getMethodsDefs()))
 	namespace.AddClass(cld.CreateClassFromDefs("", "TestClass2", getFieldsDefs(), getMethodsDefs()))
 
