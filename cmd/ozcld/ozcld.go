2f98f3ba977fa68fc881105f111a7fbcbc4a82ff
mikoto2000 <mikoto2000@gmail.com>
2017-12-29 23:15:21 +0900

    ファイル出力機能追加。
    Refs #1.

--- cmd/ozcld/ozcld.go
+++ cmd/ozcld/ozcld.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	cld "github.com/mikoto2000/ozcld"
-	"fmt"
 	"os"
 )
 
 func main() {
 	// 第一引数で渡されたファイルを読み込んでパース
-	file, _ := os.Open(os.Args[1])
-	fmt.Println(cld.Parse(file))
+	inFile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer inFile.Close()
+
+	// パースに成功したら、第二引数で渡されたファイルに出力
+	parseResult := cld.Parse(inFile)
+
+	outFile, err := os.Create(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	defer outFile.Close()
+	outFile.Write(([]byte)(parseResult))
 }
