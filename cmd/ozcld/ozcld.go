4ed1dcf4d0198232a8139fd5678bd59f27539029
mikoto2000 <mikoto2000@gmail.com>
2017-12-30 03:03:41 +0900

    コマンドライン引数パース処理追加。
    Refs #1.

--- cmd/ozcld/ozcld.go
+++ cmd/ozcld/ozcld.go
@@ -2,24 +2,59 @@ package main
 
 import (
 	cld "github.com/mikoto2000/ozcld"
+	"flag"
+	"fmt"
 	"os"
 )
 
+func printUsage() {
+	fmt.Println("Usage: ozcld [OPTIONS] INFILE")
+	flag.PrintDefaults()
+}
+
+func printStdout(message string) {
+	fmt.Println(message)
+}
+
 func main() {
-	// 第一引数で渡されたファイルを読み込んでパース
-	inFile, err := os.Open(os.Args[1])
+	flag.Usage = printUsage
+
+	// 引数パース
+	outFilePath := flag.String("o", "", "output file(default: stdout)")
+	isShowHelp := flag.Bool("h", false, "show help(default: false)")
+	flag.Parse()
+
+	// コマンドライン引数不正の場合、 Usage を出力して終了
+	if flag.NArg() != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// ヘルプ表示
+	if *isShowHelp {
+		flag.Usage()
+		os.Exit(0)
+	}
+
+	// 入力ファイルを読み込んでパース
+	inFilePath := flag.Arg(0)
+	inFile, err := os.Open(inFilePath)
 	if err != nil {
 		panic(err)
 	}
 	defer inFile.Close()
 
-	// パースに成功したら、第二引数で渡されたファイルに出力
+	// パースに成功したら、-o オプションで渡されたファイルに出力
+	// -o が無ければ、標準出力に出力する。
 	parseResult := cld.Parse(inFile)
-
-	outFile, err := os.Create(os.Args[2])
-	if err != nil {
-		panic(err)
+	if *outFilePath == "" {
+		printStdout(parseResult)
+	} else {
+		outFile, err := os.Create(*outFilePath)
+		if err != nil {
+			panic(err)
+		}
+		defer outFile.Close()
+		outFile.Write(([]byte)(parseResult))
 	}
-	defer outFile.Close()
-	outFile.Write(([]byte)(parseResult))
 }
